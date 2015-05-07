fcd586fdd007ebca0f2f022bfda306c3e8e2ab98
Thomaz Feitoza <thomaz@gerencianet.com.br>
2015-05-07 11:43:13 -0300

    Improves the sample app.

--- Example/ViewController.m
+++ Example/ViewController.m
@@ -8,18 +8,17 @@
 
 #import "ViewController.h"
 #import "GNApiSdk.h"
+#import "RegexKitLite.h"
 
 
-@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate, GNApiEndpointsDelegate>
+@interface ViewController () <GNApiEndpointsDelegate>
 
-@property (weak, nonatomic) IBOutlet UIPickerView *cardBrandPickerView;
-@property (weak, nonatomic) IBOutlet UIPickerView *methodsPickerView;
-@property (weak, nonatomic) IBOutlet UITextField *totalValueTextField;
 @property (weak, nonatomic) IBOutlet UITextField *cardNumberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *cvvTextField;
 @property (weak, nonatomic) IBOutlet UITextField *monthTextField;
 @property (weak, nonatomic) IBOutlet UITextField *yearTextField;
-@property (weak, nonatomic) IBOutlet UITextField *cvvTextField;
-@property (weak, nonatomic) IBOutlet UILabel *paymentTokenLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *cardBrandControl;
+@property (weak, nonatomic) IBOutlet UILabel *serverResponseLabel;
 
 @property (strong, nonatomic) NSArray *cardBrands;
 @property (strong, nonatomic) GNPaymentMethod *paymentMethod;
@@ -31,18 +30,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _cardBrands = @[kGNMethodNameAmex, kGNMethodNameAura, kGNMethodNameDiners, kGNMethodNameDiscover, kGNMethodNameElo, kGNMethodNameJCB, kGNMethodNameMasterCard, kGNMethodNameVisa];
-    _cardBrandPickerView.dataSource = self;
-    _cardBrandPickerView.delegate = self;
-    [_cardBrandPickerView selectRow:3 inComponent:0 animated:NO];
     
-    _methodsPickerView.dataSource = self;
-    _methodsPickerView.delegate = self;
-    
-    
+    /*
+     * SET YOUR CREDENTIALS TO USE THE TEST APP
+     */
     GNConfig *gnConfig = [[GNConfig alloc] init];
-    gnConfig.clientId = @"Client_Id_b2aed0d7c655ea4bfe69b38375f3f56dc723ece8";
-    gnConfig.clientSecret = @"Client_Secret_59092c0be17ed15ce35e63cef42ae7e6cf03ed75";
+    gnConfig.clientId = @"YOUR-CLIENT-ID";
+    gnConfig.clientSecret = @"YOUR-CLIENT-SECRET";
     gnConfig.grantType = kGNConfigGrantTypeClientCredentials;
     gnConfig.sandbox = YES;
     
@@ -53,82 +47,56 @@
 
 # pragma mark - IBActions
 
-- (IBAction)getMethodsButtonPressed {
-    NSString *methodName = _cardBrands[[_cardBrandPickerView selectedRowInComponent:0]];
-    NSNumber *totalValue = [[NSNumberFormatter alloc] numberFromString:_totalValueTextField.text];
-    GNMethod *method = [[GNMethod alloc] initWithName:methodName total:totalValue];
-    [_gnApi fetchPaymentMethods:method];
-}
-
-- (IBAction)getPaymentTokenButtonPressed {
-    GNCreditCard *creditCard = [[GNCreditCard alloc] init];
-    creditCard.number = _cardNumberTextField.text;
-    creditCard.brand = _cardBrands[[_cardBrandPickerView selectedRowInComponent:0]];
-    creditCard.expirationMonth = _monthTextField.text;
-    creditCard.expirationYear = _yearTextField.text;
-    creditCard.cvv = _cvvTextField.text;
-    
-    [_gnApi paymentTokenForCreditCard:creditCard];
-}
-
-
-# pragma mark - UIPickerViewDelegate / UIPickerViewDataSource
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    if(pickerView==_cardBrandPickerView){
-        return _cardBrands.count;
-    }
-    else if(pickerView==_methodsPickerView){
-        if(_paymentMethod){
-            return _paymentMethod.installments.count;
-        } else {
-            return 0;
-        }
-    }
-    return 0;
-}
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
-    return 1;
-}
-
-- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
-    if(!view){
-        UILabel *label = [[UILabel alloc] init];
-        label.textAlignment = NSTextAlignmentCenter;
-        view = label;
-    }
-    
-    if(pickerView==_cardBrandPickerView){
-        [(UILabel *)view setText:_cardBrands[row]];
-    } else if(pickerView==_methodsPickerView){
-        GNInstallment *installment = _paymentMethod.installments[row];
-        NSString *text = [NSString stringWithFormat:@"%dx de R$%@", installment.parcels.intValue, installment.currency];
-        [(UILabel *)view setText:text];
-    }
-    return view;
-}
-
-
-#pragma mark - GNApiEndpointsDelegate
-
-- (void)gnApiFetchPaymentMethodsFinished:(GNPaymentMethod *)paymentMethod error:(GNError *)error {
-    if(!error){
-        _paymentMethod = paymentMethod;
-        [_methodsPickerView reloadAllComponents];
-    }
-    else {
-        NSLog(@"%@", error);
+- (IBAction)onPayButtonPressed {
+    if([self validate]){
+        GNCreditCard *creditCard = [[GNCreditCard alloc] init];
+        creditCard.number = _cardNumberTextField.text;
+        creditCard.brand = _cardBrandControl.selectedSegmentIndex==0 ? kGNMethodNameVisa : kGNMethodNameMasterCard;
+        creditCard.expirationMonth = _monthTextField.text;
+        creditCard.expirationYear = _yearTextField.text;
+        creditCard.cvv = _cvvTextField.text;
+        
+        [_gnApi paymentTokenForCreditCard:creditCard];
     }
 }
+
+
+# pragma mark - GNApiEndpointsDelegate
 
 - (void)gnApiPaymentTokenForCreditCardFinished:(GNPaymentToken *)paymentToken error:(GNError *)error {
     if(!error){
-        _paymentTokenLabel.text = [NSString stringWithFormat:@"Payment token: %@", paymentToken.token];
+        _serverResponseLabel.text = [NSString stringWithFormat:@"Your payment token is %@", paymentToken.token];
     }
     else {
-        NSLog(@"%@", error.message);
+        _serverResponseLabel.text = [NSString stringWithFormat:@"Error: %d - %@", error.code.intValue, error.message];
     }
+}
+
+
+# pragma mark - Helper
+
+- (BOOL) validate {
+    if(![_cardNumberTextField.text isMatchedByRegex:@"^[0-9]{16}$"]){
+        [self alert:@"Card number must contain 16 numeric chars."];
+        return NO;
+    }
+    else if(![_cvvTextField.text isMatchedByRegex:@"^[0-9]{3}$"]){
+        [self alert:@"CVV must contain 3 numeric chars."];
+        return NO;
+    }
+    else if(![_monthTextField.text isMatchedByRegex:@"^[0-9]{2}$"]){
+        [self alert:@"Month must contain 2 numeric chars."];
+        return NO;
+    }
+    else if(![_yearTextField.text isMatchedByRegex:@"^[0-9]{4}$"]){
+        [self alert:@"Year must contain 4 numeric chars."];
+        return NO;
+    }
+    return YES;
+}
+
+- (void) alert:(NSString *)message {
+    [[[UIAlertView alloc] initWithTitle:@"Oops!" message:message delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil] show];
 }
 
 @end
